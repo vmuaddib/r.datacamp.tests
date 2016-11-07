b619560d72fc38e80cbf1a392fecb7ff7a14f50d
vmuaddib <cvali@outlook.com>
2016-11-07 23:28:29 +0100

    VS commit for matrices

--- rproject1-Test/DataCamp.Matrices.R
+++ rproject1-Test/DataCamp.Matrices.R
@@ -10,3 +10,4 @@ matrix(1:9, byrow = TRUE, nrow = 3)
 #The argument byrow indicates that the matrix is filled by the rows. If we want the matrix to be filled by the columns, we just place byrow = FALSE.
 #The third argument nrow indicates that the matrix should have three rows.
 
+
