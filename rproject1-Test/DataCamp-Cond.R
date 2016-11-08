fadd341f420d9bc14a323bdfdcccc9fd044f2d03
vmuaddib <cvali@outlook.com>
2016-11-08 15:06:43 +0100

    conf-b testing

--- rproject1-Test/DataCamp-Cond.R
+++ rproject1-Test/DataCamp-Cond.R
@@ -1,4 +1,17 @@
+#Conditional operators
 3 == (2 + 1)
 "intermediate" != "r"
 TRUE != FALSE
 "Rchitect" != "rchitect"
+
+# Compare a logical with a numeric
+TRUE == 1
+
+# Comparison of numerics
+- 6 * 5 + 2 >= -10 + 1
+
+# Comparison of character strings
+"raining" <= "raining dogs"
+
+# Comparison of logicals
+TRUE > FALSE
