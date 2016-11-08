32d076d66236ca689e47b1cdb6a81937f0100ca2
vmuaddib <cvali@outlook.com>
2016-11-08 15:29:52 +0100

    commit from condfvec

--- rproject1-Test/DataCamp-Cond.R
+++ rproject1-Test/DataCamp-Cond.R
@@ -15,3 +15,31 @@ TRUE == 1
 
 # Comparison of logicals
 TRUE > FALSE
+
+# The linkedin and facebook vectors have already been created for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+facebook <- c(17, 7, 5, 16, 8, 13, 14)
+
+# Popular days
+linkedin > 15
+
+# Quiet days
+linkedin <= 5
+
+# LinkedIn more popular than Facebook
+linkedin > facebook
+
+# The social data has been created for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+facebook <- c(17, 7, 5, 16, 8, 13, 14)
+views <- matrix(c(linkedin, facebook), nrow = 2, byrow = TRUE)
+
+# When does views equal 13?
+views == 13
+
+# When is views less than or equal to 14?
+views <= 14
+
+#------------------------------------------------------------
+# How often does facebook equal or exceed linkedin times two?
+sum(facebook >= 2 * linkedin)
