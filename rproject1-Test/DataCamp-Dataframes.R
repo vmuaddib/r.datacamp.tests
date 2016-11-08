f072e9b715b9e379ca7417eec5bf7652b5332c09
vmuaddib <cvali@outlook.com>
2016-11-08 02:05:00 +0100

    commit form branch

---
+++ rproject1-Test/DataCamp-Dataframes.R
@@ -0,0 +1,7 @@
+#You may remember from the chapter about matrices that all the elements that you put in a matrix should be of the same type. Back then, your data set on Star Wars only contained numeric elements.
+        #When doing a market research survey, however, you often have questions such as:
+        #'Are your married?' or 'yes/no' questions(logical)
+        #'How old are you?'(numeric)
+        #'What is your opinion on this product?' or other 'open-ended' questions(character)
+        #...
+#A data frame has the variables of a data set as columns and the observations as rows. 
